7ebffaad80bd84b691b25bcff122fe28418d91a2
Dawid <lukasiewicz@onet.com.pl>
2022-10-19 00:11:21 +0200

    LibInterface class init

---
+++ zalazek/inc/LibInterface.hh
@@ -0,0 +1,25 @@
+#ifndef LIBINTERFACE
+#define LIBINTERFACE
+
+#include <string>
+#include <dlfcn.h>
+
+#include "Interp4Command.hh"
+#include "MobileObj.hh"
+
+enum RTLD_mode {lazy=RTLD_LAZY, now=RTLD_NOW, binding_mask=RTLD_BINDING_MASK, noload=RTLD_NOLOAD, deepbind=RTLD_DEEPBIND};
+
+class LibInterface
+{
+private:
+    void *_LibHandler;
+    std::string _CmdName;
+    Interp4Command *(*_pCreateCmd)(void);
+    RTLD_mode _LibHndMode;
+public:
+    LibInterface(const char *LibName, RTLD_mode mode);
+    ~LibInterface();
+};
+
+
+#endif
