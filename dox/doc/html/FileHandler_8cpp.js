b195ddbab702ac35b9febbdfd502404011bc94a3
Dawid Łukasiewicz <lukasiewicz@onet.com.pl>
2022-11-27 03:06:46 +0100

    removing dox

--- dox/doc/html/FileHandler_8cpp.js
+++
@@ -1,6 +0,0 @@
-var FileHandler_8cpp =
-[
-    [ "BUFF", "FileHandler_8cpp.html#a624fd5af921a5266e2d0304a645298ba", null ],
-    [ "ExecPreprocesor", "FileHandler_8cpp.html#adc2fb4c00edb0bf25c742f6fb3d55e96", null ],
-    [ "ReadFile", "FileHandler_8cpp.html#ae98290dceda14c563cc55e5d85b91a2f", null ]
-];
--- dox/doc/html/classScene.js
+++
@@ -1,8 +0,0 @@
-var classScene =
-[
-    [ "Scene", "classScene.html#ad10176d75a9cc0da56626f682d083507", null ],
-    [ "~Scene", "classScene.html#a3b8cec2e32546713915f8c6303c951f1", null ],
-    [ "AddMobileObj", "classScene.html#abe3a9e51d9fedc1c22f9ddc3c3070b9c", null ],
-    [ "FindMobileObj", "classScene.html#aa70caf80f7ff68d4b2a6d69d769cc287", null ],
-    [ "_MobileObj", "classScene.html#add1e13d47ef08db0634cde4fde9671f0", null ]
-];
